b57d2eae74e7b8bf9f3a7d9bd8136764897158cf
Cheng Jun Yuan <supergambit1@gmail.com>
2019-12-24 17:26:22 +0800

    Update and rename vigenere WIP jy.c to vigenere jy.c

--- vigenere jy.c
+++ vigenere jy.c
@@ -4,6 +4,22 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+int shift(char c) // Nice function to convert
+{
+    if (c >= 'a' && c <= 'z') // For lowercase letters
+    {
+        return(c - 'a');
+    }
+    else if (c >= 'A' && c <= 'Z') // For uppercase letters
+    {
+        return(c - 'A');
+    }
+    else
+    {
+        return(c);
+    }
+}
+
 int main(int argc, string argv[])
 {
     if (argc == 2) // Checks that there are two arguments
@@ -14,23 +30,27 @@ int main(int argc, string argv[])
             else
             {
                 printf("Usage ./vigenere keyword\n");
-                exit(0); // Ends program
+                exit(1); // Ends program
             }
         }
         // Now I need to store it as an array of digits
         
-        int cipher = atoi(argv[1]); // Stores cipher as integer data type
+        int length = strlen(argv[1]); // Finds length of cipher
         string input = get_string("plaintext: "); // Gets message to be encrypted
+        
         printf("ciphertext: ");
+        int counter = 0;
         for (int i = 0, n = strlen(input); i < n; i++) // Encrypts message
         {
             if (input[i] >= 'a' && input[i] <= 'z') // For lowercase letters
             {
-                printf("%c", ((input[i] + cipher - 'a') % 26) + 'a');
+                counter = counter + 1;
+                printf("%c", (((input[i] + shift(argv[1][((counter - 1) % length)]) - 'a') % 26) + 'a'));
             }
             else if (input[i] >= 'A' && input[i] <= 'Z') // For uppercase letters
             {
-                printf("%c", ((input[i] + cipher - 'A') % 26) + 'A');
+                counter = counter + 1;
+                printf("%c", (((input[i] + shift(argv[1][((counter - 1) % length)]) - 'A') % 26) + 'A'));
             }
             else // For non-letters
             {
@@ -42,6 +62,6 @@ int main(int argc, string argv[])
     else // Ensures there is only one other argument
     {
         printf("Usage ./vigenere keyword\n");
-        return(1);
+        return (1);
     }
 }
